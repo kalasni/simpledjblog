bc5fe7188fe0dfafc26918b7df51cbfb037c7a0f
kalasni <kalassni@gmail.com>
2017-11-17 20:43:52 +0100

    Added Comment model and add_comment_to_post template to allow users add comments

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,15 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
 
     class Meta:  # It tells Django which model should be used to create this form
         model = Post
         fields = ('title', 'text', )
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
